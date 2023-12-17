d98e29d064b6c902000f181912f848a9dbd89688
Vivek Kamble <mrvivekkamble8@gmail.com>
2023-12-18 01:39:00 +0530

    index page added

--- index.php
+++ index.php
@@ -3,11 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Farma Tools Rent</title>
+    <link rel="stylesheet" href="Css/Index.css">
 </head>
 <body>
-<a href="Customer_login.php">Customer Login</a><br>
-    <a href="Seller_login.php">Seller Login</a>
+    <section class="section1">
+
+        <div class="logo">
+                    <h1><span class="farm">Farm </span><span class="Tools"> Tools </span><span class="Rent"> Rent</span></h1>
+        </div>
+        <div class="navbar">
+                <div class="menu" id="menu">
+                    <a href="#">About Us</a>
+                    <a href="Customer_login.php">Customer Login</a>
+                    <a href="Seller_login.php">Seller Login</a>
+                    <!-- <a href="#">Create Account</a> -->
+                <div class="dropdown">
+                    <button class="dropbtn">Create Account
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="Customer_ragistration.php">Customer Signup</a>
+                        <a href="Seller_ragistration.php">Seller Signup</a>
+                    </div>
+                </div>
+                </div>
+                <div class="icon" onclick="toggleMenu()">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </div>
+            </div>
+    </section>
+    <section class="section2">
+        <div class="s-2">
+            <div class="s-2-1">
+                <div class="s-2-1-1"><a href="#"><h1>Rent Your Farming Equipments</h1></a></div>
+                <div class="s-2-1-2"><a href="#"><h1>Buy on Rent</h1></a></div>
+            </div>
+            <div class="s-2-2">
+                
+                <img src="Images/index.png" alt="index image">
+            </div>
+        </div>
+    </section>
+    <section class="section3">
+        <h1>hii</h1>
+    </section>
+
     
+
+
+
+
+
+    <script src="JavaScript/Index.js"></script>
+
 </body>
 </html>
